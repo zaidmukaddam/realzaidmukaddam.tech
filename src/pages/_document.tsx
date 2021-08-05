3c8465570ed0b3b637a6aecf9fb9aec2115d7c3b
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2021-08-05 10:46:12 +0530

    Changed meta theme colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,10 +17,10 @@ class RealZaidMukaddamDotTech extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="mask-icon" href="/favicons//safari-pinned-tab.svg" color="#161b22" />
-          <meta name="msapplication-TileColor" content="#161b22" />
-          <meta name="theme-color" content="#161b22" />
-          <meta property="og:color" content="#161b22" />
+          <link rel="mask-icon" href="/favicons//safari-pinned-tab.svg" color="#114d9b" />
+          <meta name="msapplication-TileColor" content="#114d9b" />
+          <meta name="theme-color" content="#114d9b" />
+          <meta property="og:color" content="#114d9b" />
           <meta property="og:type" content="website" />
           <meta property="og:image" content="https://realzaidmukaddam.tech/icons/icon-128.png" />
 
